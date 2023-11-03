ef5292491c06beed5d47ac3c15ea780712ce7211
imvenx <imvenx@proton.me>
2023-11-03 12:58:04 +0100

    add arcane

--- src/App.vue
+++ src/App.vue
@@ -13,31 +13,15 @@
 </template>
 
 <script lang="ts" setup>
+import { Arcane } from 'arcanepad-web-sdk';
 import { onMounted, onBeforeUnmount, ref } from 'vue';
+
 const gameLoaded = ref(false);
-
-
-// Function to dynamically load a script
-const loadScript = (src: any) => {
-  return new Promise((resolve, reject) => {
-    // Avoid reloading the script if it's already in the document
-    if (document.querySelector(`script[src="${src}"]`)) {
-      return resolve();
-    }
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = true;
-    script.onload = () => resolve(script);
-    script.onerror = () => reject(new Error(`Failed to load the script: ${src}`));
-    document.head.appendChild(script);
-  });
-};
 
 onMounted(async () => {
 
   ; (window as any).$ = {};
 
-  // List your scripts here
   const scripts = [
     'js/jsfxr.js',
     'js/util.js',
@@ -56,10 +40,8 @@ onMounted(async () => {
     'js/levelpop.js',
     'js/button.js',
     'js/game.js',
-    'js/socketio.js',
-    'js/networking.js',
-    'js/viewer.js',
-    // Make sure to add any additional scripts you may have
+    // 'js/socketio.js',
+    // 'js/networking.js',
   ];
 
   try {
@@ -71,44 +53,28 @@ onMounted(async () => {
   } catch (error) {
     console.error('Error occurred while loading scripts:', error);
   }
+
+  Arcane.init()
+
+  await Arcane.arcaneClientInitialized()
+    ; (window as any).Arcane = Arcane;
+  await loadScript('js/viewer.js')
 });
 
-onBeforeUnmount(() => {
-  // Perform any necessary cleanup, such as removing global variables or intervals
-});
+const loadScript = (src: any) => {
+  return new Promise<void | HTMLScriptElement>((resolve, reject) => {
+    if (document.querySelector(`script[src="${src}"]`)) {
+      return resolve();
+    }
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    script.onload = () => resolve(script);
+    script.onerror = () => reject(new Error(`Failed to load the script: ${src}`));
+    document.head.appendChild(script);
+  });
+};
+
 </script>
 
-<style scoped>
-body {
-  background: #080808;
-}
-
-#wrap {
-  background: #222;
-  box-shadow: inset 0 0 0 1px #404040, 0 0 0 1px #000;
-  left: 50%;
-  opacity: 0;
-  padding: 10px;
-  position: absolute;
-  top: 50%;
-  transition: opacity 1.5s;
-}
-
-.loaded #wrap {
-  opacity: 1;
-}
-
-#wrap-inner {
-  background: #000;
-  overflow: hidden;
-  position: relative;
-}
-
-canvas {
-  cursor: crosshair;
-  display: block;
-  left: 0;
-  position: absolute;
-  top: 0;
-}
-</style>
+<style scoped></style>
